942b0f987d8a3cffaa8a29cb00d7f74ec1bbd8d3
codejoncode <jonathanjholloway@gmail.com>
2019-06-06 18:07:46 -0500

    changes

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const server = require('./server.js');
 
 const PORT = process.env.PORT || 4001;
 
+server.get("/", (req, res) => {
+    res.send("Hello World")
+  });
+
 server.listen(PORT, () => {
     console.log (`Server is listening on port ${PORT}`);
 })
