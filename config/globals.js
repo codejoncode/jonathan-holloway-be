937ced6ffb21127679df1e5cc0305a212dc1a5b0
codejoncode <jonathanjholloway@gmail.com>
2019-06-06 17:58:20 -0500

    made a change where only a certain username is able to be selected for registration and login

--- config/globals.js
+++ config/globals.js
@@ -3,10 +3,6 @@ const nodeMailerPort = process.env.NODEMAILER_PORT;
 const nodeMailerUser = process.env.NODEMAILER_USER;
 const nodeMailerPass = process.env.NODEMAILER_PASS;
 const frontEndUrl = process.env.FRONTEND_URL;
-const oAuthClientId = process.env.OAUTH_CLIENT_ID;
-const oAuthClientSecret = process.env.OAUTH_CLIENT_SECRET; 
-const oAuthRefreshToken = process.env.OAUTH_REFRESH_TOKEN;
-const oAuthAccessToken = process.env.OAUTH_ACCESS_TOKEN; 
 const personal = process.env.EMAILPERSONAL;
 
 
@@ -20,10 +16,6 @@ module.exports = {
     nodeMailerPort,
     nodeMailerUser,
     nodeMailerPass,
-    oAuthClientId,
-    oAuthClientSecret,
-    oAuthRefreshToken,
-    oAuthAccessToken,
     frontEndUrl,
     personal,
     numOfHashes,
--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -19,7 +19,10 @@ const {
 router.post("/register", requestClientIP, (req, res) => {
  /*Hash the passwords*/
   const credentials = req.body; 
-  console.log(credentials)
+  if (req.body.username !== mrHollowaySE){
+    return res.status(401).json({error: "Only the developer of this application is permitted to register. Backend was only created for emailing and blog purposes."})
+ }
+
   const hash = bcrypt.hashSync(credentials.password, numOfHashes);
   credentials.password = hash; 
   db
@@ -33,7 +36,9 @@ router.post("/login", async (req,res) => {
   let user = await db.getUserByUsername(req.body.username);  // returns an array 
   user = user[0]; 
   const credentials = req.body;
-
+  if (req.body.username !== mrHollowaySE){
+    return res.status(401).json({error: "Only the developer of this application is permitted to login. Backend was only created for emailing and blog purposes."})
+ }
   if (!user || !bcrypt.compareSync(credentials.password, user.password)){
       return res.status(401).json({error: 'Incorrect credntials'});
   } else {
@@ -41,6 +46,8 @@ router.post("/login", async (req,res) => {
 
       return res.status(200).json({token});
   }
+
+  
 })
 
 module.exports = router; 
