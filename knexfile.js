158048dd8681065c813918e09af61f13701bc87c
codejoncode <jonathanjholloway@gmail.com>
2019-06-07 21:13:01 -0500

    removed clientIPrequest off the register

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,6 @@
 // Update with your config settings.
 require('dotenv').config();
-// const {Client } = require('pg');
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
 
-// client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
 
 const localPg = {
   host: process.env.HOST,
