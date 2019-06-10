e70f5a948f4b1a22219272fd7b816d6a4b6b6a25
codejoncode <jonathanjholloway@gmail.com>
2019-06-09 20:03:29 -0500

    had to change the size of the string for features description and technologies

--- db/migrations/20190609185453_project_table.js
+++ db/migrations/20190609185453_project_table.js
@@ -2,10 +2,10 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('projects', function(tbl){
         tbl.increments()
-        tbl.string("features")
+        tbl.string("features", 2000)
         tbl.string("title").notNullable()
-        tbl.string("description")
-        tbl.string("technologies")
+        tbl.string("description", 2000)
+        tbl.string("technologies", 2000)
         tbl.string("image")
         tbl.string("gif")
         tbl.string("githubUrl")
